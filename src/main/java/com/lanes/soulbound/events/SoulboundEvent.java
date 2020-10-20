1b98cd8ebc91ca1343911231bd76c6080d08ca96
Extegral <VictorShadow-Integral@yandex.ru>
2020-10-20 20:34:58 +0200

    Fixed issues of static storage and redundant checks
    Also changed handler priority to work with Curios

--- src/main/java/com/lanes/soulbound/events/SoulboundEvent.java
+++ src/main/java/com/lanes/soulbound/events/SoulboundEvent.java
@@ -14,57 +14,28 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 @Mod.EventBusSubscriber(modid = Soulbound.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class SoulboundEvent
-{
-    public static SoulboundHandler handler;
-    public static Boolean dead = false;
+public class SoulboundEvent {
 
+	@SubscribeEvent(priority = EventPriority.HIGH) // will be using this for overriding gravestone mods.
+	public static void retrievalEvent(LivingDropsEvent event) // runs second (before respawn)
+	{
+		if (event.getEntity() instanceof PlayerEntity) {
+			SoulboundHandler.getOrCreateSoulboundHandler((PlayerEntity) event.getEntityLiving()).filterEnchantment(event.getDrops());
+		}
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST) // will be using this for overriding gravestone mods.
-    public static void retrievalEvent(LivingDropsEvent event) // runs second (before respawn)
-    {
-        if(!event.isCanceled())
-        {
-            if(event.getEntity() instanceof PlayerEntity)
-            {
-                handler.filterEnchantment(event.getDrops());
-            }
-        }
-    }
+	}
 
-    @SubscribeEvent
-    public static void playerDeathEvent(LivingDeathEvent event) // runs first (before respawn)
-    {
-        if(!event.isCanceled())
-        {
-            if(event.getEntity() instanceof PlayerEntity)
-            {
-                dead = true;
-                handler = new SoulboundHandler(event.getEntityLiving());
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public static void itemTransferEvent(PlayerEvent.Clone event) // runs third (when respawned)
-    {
-        if(!event.isCanceled())
-        {
-            if(event.isWasDeath())
-            {
-                if(dead)
-                {
-                    if(event.getEntity() instanceof PlayerEntity)
-                    {
-                        handler.transferItems(event.getPlayer());
-                    }
-                }
-            }
-        }
-    }
-
-
+	@SubscribeEvent
+	public static void itemTransferEvent(PlayerEvent.Clone event) // runs third (when respawned)
+	{
+		if (event.isWasDeath()) {
+			if (SoulboundHandler.hasActiveSoulboundHandler(event.getPlayer())) {
+				SoulboundHandler.getSoulboundHandler(event.getPlayer()).transferItems();
+			}
+		}
+	}
 
 }
--- src/main/java/com/lanes/soulbound/util/SoulboundHandler.java
+++ src/main/java/com/lanes/soulbound/util/SoulboundHandler.java
@@ -1,5 +1,6 @@
 package com.lanes.soulbound.util;
 
+import com.google.common.collect.Lists;
 import com.lanes.soulbound.config.SoulboundConfig;
 import com.lanes.soulbound.config.SoulboundGlobals;
 import com.lanes.soulbound.lists.EnchantmentList;
@@ -21,109 +22,135 @@ import java.util.*;
 
 public class SoulboundHandler
 {
-    public static LivingEntity entity;
-    public ArrayList<ItemStack> finalItems = new ArrayList<>();
-    public ArrayList<ItemEntity> drops = new ArrayList<>();
+	private static HashMap<PlayerEntity, SoulboundHandler> handlerMap = new HashMap<>();
+	public PlayerEntity player;
+	public ArrayList<ItemStack> finalItems = new ArrayList<>();
+	public ArrayList<ItemEntity> drops = new ArrayList<>();
 
-    public SoulboundHandler(LivingEntity entity)
-    {
-        SoulboundHandler.entity = entity;
-    }
+	public static SoulboundHandler getOrCreateSoulboundHandler(PlayerEntity player) {
+		if (hasActiveSoulboundHandler(player))
+			return getSoulboundHandler(player);
+		else
+			return createSoulboundHandler(player);
+	}
 
-    public void filterEnchantment(Collection<ItemEntity> retrievedDrops)
-    {
-        for(ItemEntity retrievedDrop : retrievedDrops)
-        {
-            ItemStack item = retrievedDrop.getItem();
-            if(item.isEnchanted() && EnchantmentHelper.getEnchantments(item).containsKey(EnchantmentList.SOULBOUND.get()))
-            {
-                int level = EnchantmentHelper.getEnchantmentLevel(EnchantmentList.SOULBOUND.get(), item);
-                double chance = SoulboundGlobals.saveChance + (SoulboundGlobals.additiveSaveChance * (level - 1));
-                double rng = Math.random();
-                if(rng < chance)
-                {
-                    finalItems.add(itemEditor(item).copy());
-                    retrievedDrop.remove();
-                }
-            }
-        }
-    }
+	public static SoulboundHandler getSoulboundHandler(PlayerEntity player) {
+		return handlerMap.get(player);
+	}
 
-    public ItemStack itemEditor(ItemStack item)
-    {
-        int level = EnchantmentHelper.getEnchantmentLevel(EnchantmentList.SOULBOUND.get(), item);
-        if(SoulboundGlobals.durabilityDrop)
-        {
-            double minimum = SoulboundGlobals.minimumDurabilityDrop - (SoulboundGlobals.additiveDurabilityDrop  * (level - 1));
-            if(minimum < 0) { minimum = 0; }
-            double maximum = SoulboundGlobals.maximumDurabilityDrop - (SoulboundGlobals.additiveDurabilityDrop  * (level - 1));
-            if(maximum < 0) { maximum = 0; }
-            double mode = SoulboundGlobals.modeDurabilityDrop - (SoulboundGlobals.additiveDurabilityDrop * (level - 1));
-            if(mode < 0) { mode = 0; }
+	public static SoulboundHandler createSoulboundHandler(PlayerEntity player) {
+		SoulboundHandler newHandler = new SoulboundHandler(player);
+		handlerMap.put(player, newHandler);
+		return newHandler;
+	}
 
-            int newDurability = (int) (item.getMaxDamage() * triangularDistribution(minimum, maximum, mode));
-            if(item.attemptDamageItem(newDurability, entity.getRNG(), (ServerPlayerEntity) entity))
-            {
-                if (entity instanceof PlayerEntity) {
-                    ((PlayerEntity)entity).addStat(Stats.ITEM_BROKEN.get(item.getItem()));
-                }
+	public static boolean hasActiveSoulboundHandler(PlayerEntity player) {
+		return handlerMap.get(player) != null;
+	}
 
-                if(SoulboundGlobals.breakItemOnZeroDurability)
-                {
-                    item.setDamage(0);
-                    return ItemStack.EMPTY;
-                }
-                item.setDamage(item.getMaxDamage() - 1);
-            }
+	private SoulboundHandler(PlayerEntity playerIn)
+	{
+		this.player = playerIn;
+	}
 
-        }
-        double chance = SoulboundGlobals.dropLevel - (SoulboundGlobals.additiveDropChance * (level - 1));
-        if(!(Math.random() < chance))
-        {
-            return item;
-        }
-        if(level > 1)
-        {
-            Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(item);
-            enchantments.remove(EnchantmentList.SOULBOUND.get());
-            enchantments.put(EnchantmentList.SOULBOUND.get(), level - 1);
-            EnchantmentHelper.setEnchantments(enchantments, item);
-        }
-        else
-        {
-            Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(item);
-            enchantments.remove(EnchantmentList.SOULBOUND.get());
-            EnchantmentHelper.setEnchantments(enchantments, item);
-        }
-        return item;
-    }
+	public void filterEnchantment(Collection<ItemEntity> retrievedDrops)
+	{
+		List<ItemEntity> needsRemoval = Lists.newArrayList();
+		for(ItemEntity retrievedDrop : retrievedDrops)
+		{
+			ItemStack item = retrievedDrop.getItem();
+			if(item.isEnchanted() && EnchantmentHelper.getEnchantments(item).containsKey(EnchantmentList.SOULBOUND.get()))
+			{
+				int level = EnchantmentHelper.getEnchantmentLevel(EnchantmentList.SOULBOUND.get(), item);
+				double chance = SoulboundGlobals.saveChance + (SoulboundGlobals.additiveSaveChance * (level - 1));
+				double rng = Math.random();
+				if(rng < chance)
+				{
+					this.finalItems.add(this.itemEditor(item).copy());
+					needsRemoval.add(retrievedDrop);
+				}
+			}
+		}
 
-    public double triangularDistribution(double a, double b, double c) { // where a = min, b = max, c = mode
-        // 25 , 50, 33
-        double F = (c - a) / (b - a); // f = (.33 - .25) / (.50 - .25), f = .32
-        double rand = Math.random();
-        if (rand < F) { // assuming rand = .20
-            return a + Math.sqrt(rand * (b - a) * (c - a)); // RETURNS .25 + sqrt((.20 * (.50 - .25) * (.33 - .25)) .313 (Nearing .33 with larger values, with lesser values it nears .25)
-        } else { // assuming rand = .50
-            return b - Math.sqrt((1 - rand) * (b - a) * (b - c)); // RETURNS .50 - sqrt((1 - .50) * (.50 - .25) * (.50 - .33)) .354 (Nearing .33 with lesser values, with larger values it nears .50)
-        }
-    }
+		needsRemoval.forEach(dropItem -> {
+			retrievedDrops.remove(dropItem);
+		});
+	}
 
-    public void transferItems(PlayerEntity player)
-    {
-        boolean breakonce = false;
-        if(finalItems.isEmpty())
-        {
-            return;
-        }
-        for (Object finalItem : finalItems) {
-            ItemStack item = (ItemStack) finalItem;
-            if(item == (ItemStack.EMPTY) && !breakonce)
-            {
-                player.playSound(SoundEvents.ENTITY_ITEM_BREAK, 1.0f, 1.0f); // this line of code isn't working. THIS CODE NEEDS WORK
-                breakonce = true;
-            }
-            player.inventory.addItemStackToInventory(item);
-        }
-    }
+	public ItemStack itemEditor(ItemStack item)
+	{
+		int level = EnchantmentHelper.getEnchantmentLevel(EnchantmentList.SOULBOUND.get(), item);
+		if(SoulboundGlobals.durabilityDrop)
+		{
+			double minimum = SoulboundGlobals.minimumDurabilityDrop - (SoulboundGlobals.additiveDurabilityDrop  * (level - 1));
+			if(minimum < 0) { minimum = 0; }
+			double maximum = SoulboundGlobals.maximumDurabilityDrop - (SoulboundGlobals.additiveDurabilityDrop  * (level - 1));
+			if(maximum < 0) { maximum = 0; }
+			double mode = SoulboundGlobals.modeDurabilityDrop - (SoulboundGlobals.additiveDurabilityDrop * (level - 1));
+			if(mode < 0) { mode = 0; }
+
+			int newDurability = (int) (item.getMaxDamage() * this.triangularDistribution(minimum, maximum, mode));
+			if(item.attemptDamageItem(newDurability, this.player.getRNG(), (ServerPlayerEntity) this.player))
+			{
+				if (this.player instanceof PlayerEntity) {
+					this.player.addStat(Stats.ITEM_BROKEN.get(item.getItem()));
+				}
+
+				if(SoulboundGlobals.breakItemOnZeroDurability)
+				{
+					item.setDamage(0);
+					return ItemStack.EMPTY;
+				}
+				item.setDamage(item.getMaxDamage() - 1);
+			}
+
+		}
+		double chance = SoulboundGlobals.dropLevel - (SoulboundGlobals.additiveDropChance * (level - 1));
+		if(!(Math.random() < chance))
+			return item;
+		if(level > 1)
+		{
+			Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(item);
+			enchantments.remove(EnchantmentList.SOULBOUND.get());
+			enchantments.put(EnchantmentList.SOULBOUND.get(), level - 1);
+			EnchantmentHelper.setEnchantments(enchantments, item);
+		}
+		else
+		{
+			Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(item);
+			enchantments.remove(EnchantmentList.SOULBOUND.get());
+			EnchantmentHelper.setEnchantments(enchantments, item);
+		}
+		return item;
+	}
+
+	/**
+	 * Okay I won't even try to pretend I understand your math, so I won't touch it.
+	 * @author Extegral
+	 */
+
+	public double triangularDistribution(double a, double b, double c) { // where a = min, b = max, c = mode
+		// 25 , 50, 33
+		double F = (c - a) / (b - a); // f = (.33 - .25) / (.50 - .25), f = .32
+		double rand = Math.random();
+		if (rand < F)
+			return a + Math.sqrt(rand * (b - a) * (c - a)); // RETURNS .25 + sqrt((.20 * (.50 - .25) * (.33 - .25)) .313 (Nearing .33 with larger values, with lesser values it nears .25)
+		else
+			return b - Math.sqrt((1 - rand) * (b - a) * (b - c)); // RETURNS .50 - sqrt((1 - .50) * (.50 - .25) * (.50 - .33)) .354 (Nearing .33 with lesser values, with larger values it nears .50)
+	}
+
+	public void transferItems() {
+		boolean breakonce = false;
+		if(this.finalItems.isEmpty())
+			return;
+		for (Object finalItem : this.finalItems) {
+			ItemStack item = (ItemStack) finalItem;
+			if(item == (ItemStack.EMPTY) && !breakonce)
+			{
+				this.player.playSound(SoundEvents.ENTITY_ITEM_BREAK, 1.0f, 1.0f); // this line of code isn't working. THIS CODE NEEDS WORK
+				breakonce = true;
+			}
+			this.player.inventory.addItemStackToInventory(item);
+		}
+	}
 }
